4005f6c3b99a81920c4f2e1b9ebcf62e51708db3
mafanya63 <114852343+mafanya63@users.noreply.github.com>
2022-10-30 01:36:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,68 @@
 # -*- coding: utf-8 -*-
 
-import math
-import matplotlib.pyplot as plt
+from requests import get
+import re
 import numpy as np
-import xml.etree.ElementTree as ET
+import math
+import scipy
+import matplotlib.pyplot as plt
 import os
 
 
-def func(x):
-    return - math.cos(x) * math.cos(math.pi) * math.exp(- (x - math.pi) ** 2)
+def jn(n0, kr0):
+    return scipy.special.spherical_jn(n0, kr0)
+
+
+def yn(n1, kr1):
+    return scipy.special.spherical_yn(n1, kr1)
+
+
+def hn(n2, kr2):
+    return jn(n2, kr2) + yn(n2, kr2) * 1j
+
+
+def an(n3, kr3):
+    return jn(n3, kr3) / hn(n3, kr3)
+
+
+def bn(n4, kr4):
+    return (kr4 * jn(n4 - 1, kr4) - n4 * jn(n4, kr4)) / (kr4 * hn(n4 - 1, kr4) - n4 * hn(n, kr4))
 
 
 if __name__ == '__main__':
-    xmin = -100.0
-    xmax = 100.0
-    count = 500
-    xlist = np.linspace(xmin, xmax, count)
-    ylist = [func(x) for x in xlist]
-    plt.plot(xlist, ylist)
-    plt.show()
+    url = 'https://jenyay.net/uploads/Student/Modelling/task_02_01.txt'
+    name = "file.txt"
+    with open(name, "wb") as file:
+        response = get(url)
+        file.write(response.content)
+    with open(name, 'r') as file:
+        file_lines = file.readlines()
+    Var5 = re.split(". D=|; fmin=|; fmax=|\\n", file_lines[4])
+    D = float(Var5[1])
+    f_min = int(float(Var5[2]))
+    f_max = int(float(Var5[3]))
 
-    data = ET.Element('data')
-    xdata = ET.SubElement(data, 'xdata')
-    ydata = ET.SubElement(data, 'ydata')
-    for i in range(count):
-        ET.SubElement(xdata, 'x').text = str(xlist[i])
-        ET.SubElement(ydata, 'y').text = str(ylist[i])
+    count = 1000
+    r = D / 2
+    df = np.linspace(f_min, f_max, count)
+    c = 3 * 10 ** 8
+    lmb = c / df
+    k = 2 * math.pi / lmb
+
+    summa = 0
+    for n in range(1, 80):
+        summa += ((-1) ** n * (n + 0.5) * (bn(n, k * r) - an(n, k * r)))
+    sigma = (2 * lmb ** 2 / math.pi) * abs(summa) ** 2
+
+    plt.plot(df, sigma)
+    plt.xlabel(u'2\u03C0r/\u03BB')
+    plt.ylabel(u'\u03C3/\u03C0r^2')
+    plt.show()
 
     if not os.path.exists('results'):
         os.mkdir('results')
     os.chdir(os.path.join(os.getcwd(), 'results'))
-    tree = ET.ElementTree(data)
-    tree.write('results.xml')
+
+    with open("results.txt", 'w') as file:
+        for i in range(count):
+            file.write(str(df[i]) + 4 * " " + str(sigma[i]) + "\n")
